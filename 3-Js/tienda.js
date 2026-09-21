023105efd6cfaaf5b680de45aa1dad6c393d8c12
Guillermo Pernichi <guillermopernichi@gmail.com>
2026-09-21 11:51:38 -0300

    feat: catalogos

--- 3-Js/tienda.js
+++ 3-Js/tienda.js
@@ -65,7 +65,9 @@ const productos = [
  * Mostrar un modal con el detalle del producto
  * @method mostrarModal
  */
-mostrarModal = () => {
+mostrarModal = (num) => {
+  document.getElementById("nombre-producto").innerText = productos[num].nombre;
+  document.getElementById("descripcion-producto").innerText = productos[num].description;
   document.getElementById("modal").style.display = 'block';
 }
 /**
@@ -81,11 +83,11 @@ cerrarModal = () => {
  */
 mostrarCatalogo = () => {
   let contenido = "";
-  productos.forEach((producto) => {
+  productos.forEach((producto, id) => {
     contenido += `<div>
                 <img src="https://ucc-tallerdesarrolloweb.github.io/filminas/images/ejercicios/${producto.imagen}" alt="${producto.nombre}">
                 <h3>${producto.nombre}</h3>
-                <button type="button" onclick="mostrarModal()">Ver detalle de Producto</button>
+                <button type="button" onclick="mostrarModal(${id})">Ver detalle de Producto</button>
             </div>`;
   });
   document.getElementById("catalogo").innerHTML = contenido;
